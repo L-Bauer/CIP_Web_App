acdb161b7136de6444b090563ec492fa280b0cb8
Lucas D. Bauer <lucassoccer95@gmail.com>
2023-02-21 12:39:42 -0600

    Register models to admin site

--- cip_site/catalog/admin.py
+++ cip_site/catalog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Associate, Shift, Dept, Status, CIP_Classification, CIP_Idea, Asset
 
 # Register your models here.
+admin.site.register(Associate)
+admin.site.register(Shift)
+admin.site.register(Dept)
+admin.site.register(Status)
+admin.site.register(CIP_Classification)
+admin.site.register(CIP_Idea)
+admin.site.register(Asset)
